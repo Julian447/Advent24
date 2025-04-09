30930f64d31ffe186aee40c6ddb986f0ac76df57
Nistec <julian.larsen07@gmail.com>
2025-04-09 09:38:46 +0200

    failed at day 2 part 2(redo later)

--- d2/handle_file/handle_file.cpp
+++ d2/handle_file/handle_file.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <cstddef>
+#include <cstdlib>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -23,47 +25,48 @@ void HandleFile::get_file(){
     vec_of_reports.push_back(numbers);
   }
 }
-bool isSorted(const std::vector<int>& v) {
-    if (v.size() <= 1) return true;  // Trivially sorted
+bool isSorted(std::vector<int>& v) {
+  if (v.size() <= 1) return true;  // Trivially sorted
 
-    bool ascending = true;
-    bool descending = true;
+  bool ascending = true;
+  bool descending = true;
 
-    for (size_t i = 1; i < v.size(); ++i) {
-        if (v[i] < v[i - 1]) ascending = false;
-        // if ((v[i]-v[i - 1])>3) ascending = false;
-        if (v[i] > v[i - 1]) descending = false;
-        // if ((v[i]-v[i - 1])<3) descending = false;
-    }
+  for (size_t i = 1; i < v.size(); ++i) {
+    if (v[i] < v[i - 1]) ascending = false;
+    if (v[i] > v[i - 1]) descending = false;
+  }
 
-    return ascending || descending;
+  return ascending || descending;
+}
+bool can_be_true(std::vector<int>& v) {
+  if (isSorted(v)) return true;
+
+  for (size_t i = 1; i < v.size(); i++) {
+    vector<int> mod_v = v;
+    mod_v.erase(mod_v.begin()+i);
+    if(isSorted(mod_v)) return true;
+  }
+  return false;
 }
 void HandleFile::is_safe(){
-  // vector<int> vec = {5,4,3,2,1};
-  // vector<int> vec = {1,2,3,4,5};
-  // vector<int> vec = {1,2,7,4,5};
-  // if (is_sorted(vec.begin(),vec.end()))  
-  //   cout << is_sorted(vec.begin(),vec.end()) << endl;
-  // else if (is_sorted(vec.begin(),vec.end(), greater<int>{}))
-  //   cout << is_sorted(vec.begin(),vec.end(), greater<int>{}) << endl;
-  
-  // cout << is_sorted(vec.begin(), vec.end()) || is_sorted(vec.begin(),vec.end(), greater<int>{});
 
   for (auto vec : vec_of_reports) { 
     vector<int> result {};
     adjacent_difference(vec.begin(), vec.end(), back_inserter(result));
-    // for (auto i : result) {
+    auto in_bounds = true;
+    for (int i = 1; i < vec.size(); i++) {
+      int diff = vec[i]-vec[i-1];
+      if (abs(diff) > 3) in_bounds = false;
+      if (abs(diff) == 0) in_bounds = false;
+      
+    } 
+    // for (auto i : vec) {
     //   cout << i << " ";
     // }
     // cout << endl;
-    auto in_bounds = true;
-    for (int i = 1; i < vec.size(); i++) {
-      if (vec[i]-vec[i-1] > 3 || vec[i]-vec[i-1] < -3) in_bounds = false;
-      if (vec[i]-vec[i-1] == 0) in_bounds = false;
-    } 
-
-    if (isSorted(vec) == true && in_bounds == true) {
-      sorted_count += 1;
+    // cout << "tolerate: " << tolerate << endl;
+    if (can_be_true(vec) == true && in_bounds == true) {
+      ++sorted_count;
     }
   }
   cout << "Safe: " << sorted_count << endl;
--- d2/main.cpp
+++ d2/main.cpp
@@ -1,9 +1,12 @@
 // https://adventofcode.com/2024/day/2
+// did way too badly
 #include <algorithm>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include "include/handle_file.hpp"
+
+
 
 using namespace std;
 
@@ -11,22 +14,6 @@ int main (int argc, char *argv[]) {
   HandleFile h;
   h.get_file();
   h.is_safe();
-  // cout << l.size() << endl;
-  // cout << r.size() << endl;
-  // // auto loop_length = l.size();
-  // auto loop_length = 1;
-  // for (int i = 0; i < loop_length; i++) { 
-  //   auto min_l = min_element(l.begin(),l.end());
-  //   auto min_r = min_element(r.begin(),r.end());
-  //   diff_vec.push_back(Diff(*min_l, *min_r));
-  //   cout << "Diff: " << diff_vec[i].diff << endl;
-  // }
-  // int total = 0;
-  // cout << diff_vec.size() << endl;
-  // for ( auto vec : diff_vec) {
-  //   total += vec.diff;
-  // }
-  // cout << "Total: " << total << endl;
 
   return 0;
 }
