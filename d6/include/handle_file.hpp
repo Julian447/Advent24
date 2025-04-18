9c885f3143afc24ad8b0488783a6b9e4834a64f4
Nistec <julian.larsen07@gmail.com>
2025-04-18 13:04:27 +0200

    part 1 solved

--- d6/include/handle_file.hpp
+++ d6/include/handle_file.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <tuple>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -8,10 +9,22 @@ using namespace std;
 class HandleFile {
   string str; 
   vector<string> input;
+  string direction = "up";
+  pair<int,int> position;
+  vector<pair<int, int>> guards;
+  int walked = 0;
   
   public:
     void get_file();
     void part1();
     void part2();
+    void print();
+    void change_direction();
+    void load_guards();
+    void patrol();
+    bool in_bounds(int i, int j) const {
+    return i >= 0 && i < static_cast<int>(input.size()) &&
+           j >= 0 && j < static_cast<int>(input[0].size());
+  }
 };
 
--- d6/source/handle_file.cpp
+++ d6/source/handle_file.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -13,8 +14,109 @@
 
 using namespace std;
 
+void HandleFile::print(){
+  for (auto row : input) {
+    for (auto s : row) {
+      cout << s << " ";
+    }
+    cout << endl;
+  }
+}
+void HandleFile::change_direction(){
+  if (direction == "up") direction = "right";
+  else if (direction == "right") direction = "down";
+  else if (direction == "down") direction = "left";
+  else if (direction == "left") direction = "up";
+}
+void HandleFile::load_guards(){
+  for (size_t i = 0; i < input.size(); ++i) {
+    for (size_t j = 0; j < input[i].size(); ++j) {
+      if (input[i][j] == '#') guards.push_back(pair<int, int>(i,j));
+      if (input[i][j] == '^') position = pair<int,int>(i,j);
+    }
+  }
+}
+void HandleFile::patrol(){
+  size_t i = position.first;
+  size_t j = position.second;
+
+  while(true){ 
+    if (input[i][j] != 'X')
+      walked++;
+    input[i][j] = 'X';
+
+    if (direction == "left") {
+      --j;
+      if (!in_bounds(i, j-1)){
+        input[i][j] = 'X';
+        walked++;
+        break;
+      }
+      if (input[i][j-1] == '#') {
+        input[i][j] = '^';
+        change_direction();
+        print();
+        cout << "Walked: " << walked << endl;
+      }
+    }
+    else if (direction == "up") {
+      --i;
+      if (!in_bounds(i-1, j)){
+        input[i][j] = 'X';
+        walked++;
+        break;
+      }
+      if (input[i-1][j] == '#') {
+        input[i][j] = '>';
+        change_direction();
+        print();
+        cout << "Walked: " << walked << endl;
+      }
+    }
+    else if (direction == "right") {
+      ++j;
+      if (!in_bounds(i, j+1)){
+        input[i][j] = 'X';
+        walked++;
+        break;
+      }
+      if (input[i][j+1] == '#') {
+        input[i][j] = 'v';
+        change_direction();
+        print();
+        cout << "Walked: " << walked << endl;
+      }
+    }
+    else if (direction == "down") {
+      ++i;
+      if (!in_bounds(i+1, j)){
+        input[i][j] = 'X';
+        walked++;
+        break;
+      }
+      if (input[i+1][j] == '#') {
+        input[i][j] = '<';
+        change_direction();
+        print();
+        cout << "Walked: " << walked << endl;
+      }
+    }
+  }
+}
 
 void HandleFile::part1(){
+  print();
+  load_guards();
+  cout << endl;
+  patrol();
+  cout << endl;
+  // for (auto guard : guards) {
+  //   cout << guard.first << " " << guard.second << endl;
+  // }
+  // cout << "position: " << position.first << " " << position.second << endl;
+
+  print();
+  cout << "Walked: " << walked << endl;
 }
 
 void HandleFile::part2(){
