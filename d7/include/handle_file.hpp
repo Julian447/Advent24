8a23b1866e64191bb9c890a9fb80cea8ace07eab
Nistec <julian.larsen07@gmail.com>
2025-04-19 13:27:20 +0200

    gotta remember to turn all ints something with more bits :)

--- d7/include/handle_file.hpp
+++ d7/include/handle_file.hpp
@@ -1,6 +1,5 @@
+#include <cstdint>
 #include <string>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 using namespace std;
@@ -9,16 +8,14 @@ using namespace std;
 class HandleFile {
   string str; 
   vector<string> input;
-  string direction = "up";
-  pair<int,int> position;
-  vector<pair<int, int>> guards;
-  int walked = 0;
-  
+
   public:
     void get_file();
     void part1();
     void part2();
     void print();
+    // bool combine(int result, const vector<int>& combinators, int iterator, size_t index);
+    bool combine(uint64_t result, const vector<uint64_t>& combinators);
     bool in_bounds(int i, int j) const {
     return i >= 0 && i < static_cast<int>(input.size()) &&
            j >= 0 && j < static_cast<int>(input[0].size());
--- d7/source/handle_file.cpp
+++ d7/source/handle_file.cpp
@@ -1,6 +1,8 @@
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <iterator>
 #include <sstream>
@@ -17,30 +19,76 @@ void HandleFile::print(){
     cout << row << endl;
   }
 }
+
+bool HandleFile::combine(uint64_t result, const vector<uint64_t>& combinators) {
+  function<bool(uint64_t, size_t)> dfs = [&](uint64_t current, size_t index) {
+    if (index == combinators.size()) {
+      return current == result;
+    }
+
+    int next = combinators[index];
+    return dfs(current + next, index + 1) || dfs(current * next, index + 1);
+  };
+
+  // Start from the first number, begin chaining from index 1
+  return dfs(combinators[0], 1);
+}
 void HandleFile::part1(){
   // print();
   
-  auto loads = [&](string row) -> pair<int,vector<int>>{
-    vector<string> extracted;
-    stringstream ss(row);
-    int result;
-
-    while (getline(ss,str, ':')) {
-      extracted.push_back(str);
+  auto loads = [&](const string& row) -> pair<uint64_t, vector<uint64_t>> {
+    size_t colon_pos = row.find(':');
+    if (colon_pos == string::npos) {
+      throw runtime_error("Invalid input format: missing ':'");
     }
-    result = stoi(extracted[0]);
-    stringstream s(extracted[1]);
-    vector<int> combinators((istream_iterator<int>(s)), istream_iterator<int>());
+
+    uint64_t result = stoull(row.substr(0, colon_pos));
+    // cout << "Result: " << result << ", ";
+  
+    string combinator_part = row.substr(colon_pos + 1);
+    stringstream s(combinator_part);
+    vector<uint64_t> combinators((istream_iterator<uint64_t>(s)), istream_iterator<uint64_t>());
+    
+    string test = to_string(result) + ": ";
+    for (size_t i = 0; i < combinators.size(); ++i){
+      if (i == combinators.size()-1)
+        test += to_string(combinators[i]);
+      else
+        test += to_string(combinators[i]) + " ";
+    }
+    if (test != row) {
+      cout << "Result: " << result << ", " << endl;
+      cout << "Combinators: ";
+      for (long long num : combinators) {
+          cout << num << " ";
+      }
+      cout << endl;
+    }
+
     return {result, combinators};
   };
+
+  uint64_t sum_result = 0;  
+  for (size_t i = 0; i < input.size(); ++i) { 
+    auto load = loads(input[i]);
   
-  int sum_result = 0;
-  auto load = loads(input[0]);
-  cout << load.first << endl;
-  for (auto i : load.second) {
-    cout << i << " ";
+    if (combine(load.first,load.second) != 0){
+      auto prev_sum_result = sum_result;
+      sum_result += load.first;
+      if ((sum_result - prev_sum_result) != load.first){
+        cout << "Result: " << load.first << " can be combined, Loads: ";
+        for (auto s : load.second) {
+          cout << s << " ";
+        }
+        cout << endl;
+        cout << sum_result << endl;
+      }
+    }
+    else {
+      // cout << "can not be combined" << endl;
+    }
   }
-  cout << endl;
+  cout << sum_result << endl;
 
 }
 
